774687fda445017968fdb5b6d57c8ff36549a7f3
syborg64 <julian.scott@epitech.eu>
2026-02-23 23:07:12 +0100

    feat(editor): implement editor feature with the app

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use bevy::prelude::*;
+#[cfg(feature = "editor")]
+use bevy_editor::EditorPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins
-            .set(ImagePlugin::default_nearest())
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "SolaceInSolitude".into(),
-                    name: Some("SolaceInSolitude".into()),
+        .add_plugins((
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "SolaceInSolitude".into(),
+                        name: Some("SolaceInSolitude".into()),
+                        ..default()
+                    }),
                     ..default()
                 }),
-                ..default()
-            }),))
+            #[cfg(feature = "editor")]
+            EditorPlugin,
+        ))
         .run();
 }
